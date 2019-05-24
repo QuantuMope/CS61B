405d302e02d27410c9c715b08dbe0269b3661ebb
QuantuMope <43973701+QuantuMope@users.noreply.github.com>
2019-05-23 21:12:42 -0700

    Complete Lab 6 minus Tests.

--- Labs/Lab_6/UnionFind.java
+++ Labs/Lab_6/UnionFind.java
@@ -1,35 +1,49 @@
+/**
+ *
+ * Implementation of UnionFind also known as Disjoint Sets,
+ * @author Andrew Choi
+ * Date: 05/23/2019
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class UnionFind {
 
-    // TODO - Add instance variables?
+    private ArrayList<Integer> vertices;
 
     /* Creates a UnionFind data structure holding n vertices. Initially, all
        vertices are in disjoint sets. */
     public UnionFind(int n) {
-        // TODO
+        vertices = new ArrayList<>(Collections.nCopies(n, -1));
     }
 
     /* Throws an exception if v1 is not a valid index. */
     private void validate(int vertex) {
-        // TODO
+        if (vertex > vertices.size()-1 || vertex < 0) {
+            String exception = String.format("The input is not valid. Please choose from 0 to %s", vertices.size()-1);
+            throw new IndexOutOfBoundsException(exception);
+        }
     }
 
     /* Returns the size of the set v1 belongs to. */
     public int sizeOf(int v1) {
-        // TODO
-        return -1;
+        int index = v1;
+        while (parent(index) >= 0) {
+            index = parent(index);
+        }
+        return Math.abs(index);
     }
 
     /* Returns the parent of v1. If v1 is the root of a tree, returns the
        negative size of the tree for which v1 is the root. */
     public int parent(int v1) {
-        // TODO
-        return -1;
+        return vertices.get(v1);
     }
 
     /* Returns true if nodes v1 and v2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO
-        return false;
+        return find(v1) == find(v2);
     }
 
     /* Connects two elements v1 and v2 together. v1 and v2 can be any valid 
@@ -38,14 +52,31 @@ public class UnionFind {
        vertex with itself or vertices that are already connected should not 
        change the sets but may alter the internal structure of the data. */
     public void union(int v1, int v2) {
-        // TODO
+        // Validates inputs.
+        validate(v1);
+        validate(v2);
+        int root1 = parent(v1);
+        int root2 = parent(v2);
+        if (sizeOf(v1) <= sizeOf(v2) ) {
+            vertices.set(root2, root1);
+        } else {
+            vertices.set(root1, root2);
+        }
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. */
     public int find(int vertex) {
-        // TODO
-        return -1;
+        int index = vertex;
+        int root = -5;
+        while (parent(index) >= 0) {
+            root = index;
+            index = parent(index);
+        }
+        if (root == -5) {
+            return parent(vertex);
+        }
+        return root;
     }
 
 }

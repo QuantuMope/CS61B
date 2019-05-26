e16c69d3625c04f39bd2ab5468865c3d15292500
QuantuMope <43973701+QuantuMope@users.noreply.github.com>
2019-05-25 21:44:22 -0700

    Start on HW2 - Percolations.

--- HW/HW2/hw2/Percolation.java
+++ HW/HW2/hw2/Percolation.java
@@ -3,5 +3,90 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private class Square {
+        private boolean open = false;
+        private boolean full = false;
+
+        public Square() {}
+
+        public Square(String input) {
+            if (input.equals("TOP")) {
+                open = true;
+                full = true;
+            }
+        }
+    }
+    private Square[][] grid;
+    private WeightedQuickUnionUF quickUnion;
+    private int lengthWidth;
+    private Square top = new Square("TOP");
+    private int openSpots;
+
+    // Create N-by-N grid with all sites initially blocked.
+    // Allowed to take N squared time.
+    public Percolation(int N) {
+        openSpots = 0;
+        grid = new Square[N][N];
+        lengthWidth = N;
+        quickUnion = new WeightedQuickUnionUF(N*N + 2);
+        for (int column = 0; column < N; column++) {
+            quickUnion.union(N*N, column);
+        }
+        for (int row = 0; row < N; row++) {
+            for (int column = 0; column < N; column++) {
+                grid[row][column] = new Square();
+            }
+        }
+    }
+
+    // Helper method to translate coordinates to a single int value.
+    private int calc(int row, int col) {
+        if (row > lengthWidth || col > lengthWidth) {
+            throw new IllegalArgumentException("Coordinates are out of range.");
+        }
+        return lengthWidth * row + col;
+    }
+
+    // Helper method to check for open adjacent connections and connect them.
+    private void connectOpens(int row, int col) {
+        if (grid[row][col+1].open && (col + 1) < lengthWidth) { quickUnion.union(calc(row, col), calc(row, col+1)); }
+        if (grid[row][col-1].open && (col - 1) < lengthWidth) { quickUnion.union(calc(row, col), calc(row, col-1)); }
+        if (grid[row+1][col].open && (row + 1) < lengthWidth) { quickUnion.union(calc(row, col), calc(row+1, col)); }
+        if (grid[row-1][col].open && (row - 1) < lengthWidth) { quickUnion.union(calc(row, col), calc(row-1, col)); }
+    }
+
+    // Open the site (row, col) if it is not open.
+    public void open (int row, int col) {
+        if (!isOpen(row, col)) {
+            openSpots += 1;
+            grid[row][col].open = true;
+            connectOpens(row, col);
+        }
+
+    }
+
+    // Is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        return grid[row][col].open;
+    }
+
+    // Is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        return grid[row][col].full;
+    }
+
+    // Return number of open sites.
+    public int numberOfOpenSites() {
+        return openSpots;
+    }
+
+    // Does the system percolate?
+    public boolean percolates() {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Percolation example = new Percolation(3);
+    }
 
 }

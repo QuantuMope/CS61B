6562bf55478fab716a3aeb79fab63dd1513c6af9
QuantuMope <43973701+QuantuMope@users.noreply.github.com>
2019-06-03 04:14:30 -0700

    Complete most of Project 2A. Finish up ChangePriority.

--- Projects/Proj2AB-HeapPQ_and_KDTree/bearmaps/ArrayHeapMinPQ.java
+++ Projects/Proj2AB-HeapPQ_and_KDTree/bearmaps/ArrayHeapMinPQ.java
@@ -22,15 +22,16 @@ public class ArrayHeapMinPQ<T extends Comparable<T>> implements ExtrinsicMinPQ<T
     private class Entry<T> {
         private T item;
         private double priority;
-        public Entry(T item, double priority) {
+        private Entry(T item, double priority) {
             this.item = item;
             this.priority = priority;
         }
     }
 
-    public ArrayHeapMinPQTest() {
+    public ArrayHeapMinPQ() {
         size = 0;
-        heap = new ArrayList<>();
+        heap = new ArrayList<>(3);
+        heap.add(null);
         index = 1;
         keys = new HashSet<>();
     }
@@ -42,29 +43,52 @@ public class ArrayHeapMinPQ<T extends Comparable<T>> implements ExtrinsicMinPQ<T
     public void add(T item, double priority){
         if (contains(item)) { throw new IllegalArgumentException("Item already exists in the PQ."); }
         keys.add(item);
-        heap.set(index, new Entry(item, priority));
+        heap.add(index, new Entry(item, priority));
         swim(index);
         index++;
         size++;
     }
 
+    /* Helper method that swaps an Entry with its parent if
+     * it has a lower value until it reaches an appropriate spot. */
     private void swim(int k) {
+        if (k == 1) { return; }
         if (heap.get(parent(k)).priority > (heap.get(k).priority)) {
-            Entry temp = heap.get(k);
-            heap.set(k, heap.get(parent(k)));
-            heap.set(parent(k), temp);
+            swap(k);
             swim(parent(k));
         }
     }
 
-    private int parent(int k) {
-        return k/2;
+    /* Helper method to swap indexed Entry with its parent. */
+    private void swap(int k) {
+        Entry temp = heap.get(k);
+        heap.set(k, heap.get(parent(k)));
+        heap.set(parent(k), temp);
     }
 
-    /* Returns true if the PQ contains the given item. */
-    public boolean contains(T item) {
-        return keys.contains(item);
+    /* Helper method that swaps an Entry with its smaller child
+     * until it reaches an appropriate spot. */
+    private void sink(int k) {
+        if (k*2 > size || (k*2+1 > size && heap.get(k*2).priority > heap.get(k).priority)) {
+            return;
+        } else if (k*2+1 > size && heap.get(k*2).priority < heap.get(k).priority) {
+            swap(k*2);
+            return;
+        }
+        if (heap.get(k*2).priority < heap.get(k*2+1).priority && heap.get(k*2).priority < heap.get(k).priority) {
+            swap(k*2);
+            sink(k*2);
+        } else if (heap.get(k*2).priority > heap.get(k*2+1).priority && heap.get(k*2+1).priority < heap.get(k).priority) {
+            swap(k*2+1);
+            sink(k*2+1);
+        }
     }
+
+    /* Helper method to obtain the parent of the indexed Entry. */
+    private int parent(int k) { return k/2; }
+
+    /* Returns true if the PQ contains the given item. */
+    public boolean contains(T item) { return keys.contains(item); }
 
     /* Returns the minimum item. Throws NoSuchElementException if the PQ is empty. */
     public T getSmallest(){
@@ -74,17 +98,39 @@ public class ArrayHeapMinPQ<T extends Comparable<T>> implements ExtrinsicMinPQ<T
 
     /* Removes and returns the minimum item. Throws NoSuchElementException if the PQ is empty. */
     public T removeSmallest() {
-        return null;
+        index -= 1;
+        size -= 1;
+        T temp = heap.get(1).item;
+        keys.remove(temp);
+        heap.set(1, null);
+        heap.set(1, heap.get(index));
+        heap.set(index, null);
+        sink(1);
+        return temp;
     }
 
     /* Returns the number of items in the PQ. */
-    public int size() {
-        return size;
-    }
+    public int size() { return size; }
 
     /* Changes the priority of the given item. Throws NoSuchElementException if the item
      * doesn't exist. */
     public void changePriority(T item, double priority) {
+        if (!contains(item)) { throw new NoSuchElementException(); }
+        heap.get(heap.indexOf(item))
 
+
+        int location = find(item);
+        heap.get(location).priority = priority;
+        swim(location);
+        sink(location);
+    }
+
+    private int find(T item) {
+        for (int i = 1; i <= size; i++) {
+            if (heap.get(i).item.equals(item)) {
+                return i;
+            }
+        }
+        return 0;
     }
 }
--- Projects/Proj2AB-HeapPQ_and_KDTree/bearmaps/ArrayHeapMinPQTest.java
+++ Projects/Proj2AB-HeapPQ_and_KDTree/bearmaps/ArrayHeapMinPQTest.java
@@ -1,4 +1,80 @@
 package bearmaps;
 
+import org.junit.Test;
+
+import java.lang.reflect.Array;
+import edu.princeton.cs.algs4.Stopwatch;
+import static org.junit.Assert.*;
+
 public class ArrayHeapMinPQTest {
+
+    @Test
+    public void removeSmallestTest() {
+        ArrayHeapMinPQ<Integer> test = new ArrayHeapMinPQ<>();
+        test.add(5,5);
+        test.add(7,7);
+        test.add(8,8);
+        test.add(4,4);
+        test.add(19,19);
+        test.add(2,2);
+        assertEquals(2, (int) test.removeSmallest());
+        assertEquals(4, (int) test.removeSmallest());
+        assertEquals(5, (int) test.removeSmallest());
+        assertFalse(test.contains(5));
+        assertEquals(7, (int) test.removeSmallest());
+        assertTrue(test.contains(19));
+        assertEquals(8, (int) test.removeSmallest());
+        assertEquals(19, (int) test.removeSmallest());
+        test.add(8,8);
+        assertEquals(8, (int) test.getSmallest());
+    }
+
+    @Test
+    public void changePriorityTest() {
+        ArrayHeapMinPQ<String> test = new ArrayHeapMinPQ<>();
+        test.add("first",1);
+        test.add("second",2);
+        test.add("third",3);
+        test.add("fourth",4);
+        test.add("fifth",5);
+        test.add("sixth",6);
+        test.changePriority("fourth", 0.5);
+        assertEquals("fourth", test.removeSmallest());
+        assertEquals("first", test.getSmallest());
+        test.changePriority("third", 1.5);
+        test.removeSmallest();
+        assertEquals("third", test.getSmallest());
+    }
+
+    @Test
+    public void timerTest() {
+        NaiveMinPQ<Integer> naive = new NaiveMinPQ<>();
+        ArrayHeapMinPQ<Integer> proper = new ArrayHeapMinPQ<>();
+
+        Stopwatch sw1 = new Stopwatch();
+        for (int i = 0; i < 10000; i++) {
+            naive.add(i, i);
+        }
+        for (int i = 0; i < 10000; i+=2) {
+            naive.changePriority(i, i*10);
+        }
+//        for (int i = 0; i < 10000; i++) {
+//            naive.removeSmallest();
+//        }
+        System.out.println("Total time elapsed: " + sw1.elapsedTime() +  " seconds.");
+
+        Stopwatch sw2 = new Stopwatch();
+        for (int i = 0; i < 10000; i++) {
+            proper.add(i, i);
+        }
+        for (int i = 0; i < 10000; i+=2) {
+            proper.changePriority(i, i*10);
+        }
+//        for (int i = 0; i < 10000; i++) {
+//            proper.removeSmallest();
+//        }
+        System.out.println("Total time elapsed: " + sw2.elapsedTime() +  " seconds.");
+    }
+
+
 }

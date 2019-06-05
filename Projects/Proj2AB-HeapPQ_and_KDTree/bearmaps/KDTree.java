c8f4a1266538c15816bd6d7237fb92c7c02a2a0d
QuantuMope <43973701+QuantuMope@users.noreply.github.com>
2019-06-05 03:35:55 -0700

    Finish outline of KDTree. Needs pruning and bad child checking.

--- Projects/Proj2AB-HeapPQ_and_KDTree/bearmaps/KDTree.java
+++ Projects/Proj2AB-HeapPQ_and_KDTree/bearmaps/KDTree.java
@@ -1,10 +1,117 @@
 package bearmaps;
 
+import java.util.List;
+import java.util.Comparator;
+import java.lang.*;
 /**
- *
+ * A KDTree approach to solving the nearest point problem.
+ * Much more efficient than the NaivePointSet.
  * @author Andrew Choi
  * Date: 06/04/2019
  */
 
-public class KDTree {
+public class KDTree implements PointSet {
+
+    private int size;
+    private List<Point> points;
+    private Node root = null;
+    private Comparator<Point> XxCC = new XxComparator();
+    private Comparator<Point> YyCC = new YyComparator();
+    private int comparatorChooserIndex = 0;
+
+    public KDTree(List<Point> points) {
+        size = 0;
+        for (Point point : points) {
+            insert(point);
+        }
+    }
+
+    private class Node {
+        private Point point;
+        private Node left;
+        private Node right;
+
+        private Node(Point input) {
+            this.point = input;
+        }
+        private Point getPoint() { return point; }
+        private double getX() { return point.getX(); }
+        private double getY() { return point.getY(); }
+    }
+
+
+    /** Comparator classes for comparing X & Y coordinates. */
+    private class XxComparator implements Comparator<Point> {
+        public int compare(Point p1, Point p2) {
+            return ((Double) p1.getX()).compareTo(p2.getX());
+        }
+    }
+
+    private class YyComparator implements Comparator<Point> {
+        public int compare(Point p1, Point p2) {
+            return ((Double) p1.getY()).compareTo(p2.getY());
+        }
+    }
+
+    private void insert(Point input) {
+        if (size == 0) {
+            size += 1;
+            root = new Node(input);
+            return;
+        }
+        insertHelper(input, root, comparatorChooser());
+    }
+
+    private Comparator<Point> comparatorChooser() {
+        if (comparatorChooserIndex == 0) { return XxCC; }
+        return YyCC;
+    }
+
+    private Node insertHelper(Point input, Node curr, Comparator<Point> cc) {
+        if (curr == null) {
+            size += 1;
+            comparatorChooserIndex = 0;
+            return new Node(input);
+        }
+        if (cc.compare(curr.getPoint(), input) > 0) {
+            comparatorChooserIndex ^= 1;
+            curr.left = insertHelper(input, curr.left, comparatorChooser());
+        } else if (cc.compare(curr.getPoint(), input) <= 0) {
+            comparatorChooserIndex ^= 1;
+            curr.right = insertHelper(input, curr.right, comparatorChooser());
+        }
+        return curr;
+    }
+
+    @Override
+    public Point nearest(double x, double y) {
+        Point inputPoint = new Point(x, y);
+        Point bestPoint = root.getPoint();
+        double best = Point.distance(bestPoint, inputPoint);
+        return nearestHelper(inputPoint, root.getPoint(), root, best, comparatorChooser());
+    }
+
+    private Point nearestHelper(Point input, Point bestPoint, Node curr, double best, Comparator<Point> cc) {
+        if (curr == null) {
+            return bestPoint;
+        }
+        if (cc.compare(curr.getPoint(), input) > 0) {
+            comparatorChooserIndex ^= 1;
+            if (Point.distance(curr.getPoint(), input) < best) {
+                best = Point.distance(curr.getPoint(), input);
+                bestPoint = curr.getPoint();
+            }
+            bestPoint = nearestHelper(input, bestPoint, curr.left, best, comparatorChooser());
+        } else if (cc.compare(curr.getPoint(), input) <= 0) {
+            comparatorChooserIndex ^= 1;
+            if (Point.distance(curr.getPoint(), input) < best) {
+                best = Point.distance(curr.getPoint(), input);
+                bestPoint = curr.getPoint();
+            }
+            bestPoint = nearestHelper(input, bestPoint, curr.right, best, comparatorChooser());
+        }
+        return bestPoint;
+    }
+
+
 }
--- Projects/Proj2AB-HeapPQ_and_KDTree/bearmaps/KDTreeTest.java
+++ Projects/Proj2AB-HeapPQ_and_KDTree/bearmaps/KDTreeTest.java
@@ -1,7 +1,52 @@
 package bearmaps;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import edu.princeton.cs.algs4.Stopwatch;
+
 import static org.junit.Assert.assertEquals;
 
+/**
+ * Tests for KDTree.
+ * @author Andrew Choi
+ * Date: 06/04/2019
+ */
+
 public class KDTreeTest {
+
+    @Test
+    public void insertTest() {
+
+        Point ins1 = new Point(2.5, 5.5);
+        Point ins2 = new Point(3.0, 5.5);
+        Point ins3 = new Point(3.0, 4.0);
+        Point ins4 = new Point(1.0, 3.4);
+        Point ins5 = new Point(6.7, 7.8);
+        Point ins6 = new Point(2.5, 1.6);
+        NaivePointSet testNaive = new NaivePointSet(List.of(ins1, ins2, ins3, ins4, ins5, ins6));
+        KDTree testKD = new KDTree(List.of(ins1, ins2, ins3, ins4, ins5, ins6));
+        assertEquals(testNaive.nearest(2.7, 3.6), testKD.nearest(2.7, 3.6));
+
+        ArrayList<Point> input = new ArrayList<>(45000);
+        for (double x = 0; x < 1000; x+=1.5) {
+            for (double y = 0; y < 1000; y+=1.5) {
+                input.add(new Point(x, y));
+            }
+        }
+        testNaive = new NaivePointSet(input);
+        testKD = new KDTree(input);
+
+        Stopwatch sw1 = new Stopwatch();
+        Point expected = testNaive.nearest(375.4, 124.76);
+        System.out.println("Naive total time elapsed: " + sw1.elapsedTime() +  " seconds.");
+
+        Stopwatch sw2 = new Stopwatch();
+        Point actual = testKD.nearest(375.4, 124.76);
+        System.out.println("KDTree total time elapsed: " + sw2.elapsedTime() +  " seconds.");
+
+        assertEquals(expected, actual);
+    }
+
 }

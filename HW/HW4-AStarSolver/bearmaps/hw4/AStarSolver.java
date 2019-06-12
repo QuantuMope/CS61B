ec85248c6062122bc025c33e3a6fa18d4139d306
QuantuMope <43973701+QuantuMope@users.noreply.github.com>
2019-06-12 04:17:22 -0700

    Fix AStar path list bug. Fix ArrayHeapMinPQ tiebreaker bug.

--- HW/HW4-AStarSolver/bearmaps/hw4/AStarSolver.java
+++ HW/HW4-AStarSolver/bearmaps/hw4/AStarSolver.java
@@ -1,29 +1,28 @@
 package bearmaps.hw4;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
 import bearmaps.proj2ab.ArrayHeapMinPQ;
 import bearmaps.proj2ab.DoubleMapPQ;
 import edu.princeton.cs.algs4.Stopwatch;
 
 /**
- * A* algorithm - Shortest path solver. Uses a MinPQ to keep track of vertex
- * travel order. Uses a HashMap to record best node distances.
+ * Memory Efficient A* algorithm - Shortest path solver.
+ * Uses a MinPQ to keep track of vertex travel order.
+ * Uses a HashMap to record best node distances.
  * @author Andrew Choi
  * Date: 06/11/2019
  */
 
-public class AStarSolver<Vertex extends Comparable<Vertex>> implements ShortestPathsSolver<Vertex> {
-    private SolverOutcome outcome;
+public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
+    private SolverOutcome outcome = null;
     private double solutionWeight;
     private List<Vertex> solution;
+    private List<Vertex> dequeuedList;
     private double timeSpent;
-    private DoubleMapPQ<Vertex> PQ;
+    private ArrayHeapMinPQ<Vertex> PQ;
     private HashMap<Vertex, Double> distances;
-    private HashSet<Vertex> dequeuedList;
+    private HashMap<Vertex, Vertex> origins;
     private int dequeues;
 
     /** Constructor which finds the solution, computing everything necessary
@@ -31,14 +30,15 @@ public class AStarSolver<Vertex extends Comparable<Vertex>> implements ShortestP
      *  Note that timeout passed in is in seconds. */
     public AStarSolver(AStarGraph<Vertex> input, Vertex start, Vertex end, double timeout) {
         Stopwatch sw = new Stopwatch();
-        PQ = new DoubleMapPQ<>();
+        PQ = new ArrayHeapMinPQ<>();
         distances = new HashMap<>();
         solution = new ArrayList<>();
-        dequeuedList = new HashSet<>();
+        dequeuedList = new ArrayList<>();
+        origins = new HashMap<>();
         dequeues = 0;
         PQ.add(start, 0.0);
         distances.put(start, 0.0);
-        boolean finished = false;
+        origins.put(start, start);
         Vertex pop = PQ.getSmallest();
 
         // In case start vertex is the end vertex.
@@ -47,10 +47,9 @@ public class AStarSolver<Vertex extends Comparable<Vertex>> implements ShortestP
             solutionWeight = 0;
             outcome = SolverOutcome.SOLVED;
             timeSpent = sw.elapsedTime();
-            finished = true;
         }
 
-        while (!finished) {
+        while (outcome == null) {
 
             // If PQ is empty, means no possible path from start to end.
             // Problem is unsolvable.
@@ -59,62 +58,71 @@ public class AStarSolver<Vertex extends Comparable<Vertex>> implements ShortestP
                 solutionWeight = 0;
                 outcome = SolverOutcome.UNSOLVABLE;
                 timeSpent = sw.elapsedTime();
-                finished = true;
             } else {
                 // Take the next best vertex from fringe.
                 pop = PQ.removeSmallest();
                 dequeuedList.add(pop);
                 dequeues++;
-                solution.add(pop);
                 solutionWeight = distances.get(pop);
-            }
+                System.out.println(dequeues);
 
-            // Monitoring timeout scenario.
+                // Monitoring timeout scenario.
             if (sw.elapsedTime() > timeout) {
                 solution = new ArrayList<>();
                 solutionWeight = 0;
                 outcome = SolverOutcome.TIMEOUT;
                 timeSpent = sw.elapsedTime();
-                finished = true;
             }
 
-            // Iterate through neighbors and record distances, and either
-            // add new vertexes to fringe or possibly change a vertex's priority.
-            List<WeightedEdge<Vertex>> neighborEdges = input.neighbors(pop);
-            for (WeightedEdge<Vertex> e : neighborEdges) {
-                if (dequeuedList.contains(e.to())) { continue; }
+                // Iterate through neighbors and record distances, and either
+                // add new vertexes to fringe or possibly change a vertex's priority.
+                List<WeightedEdge<Vertex>> neighborEdges = input.neighbors(pop);
+                for (WeightedEdge<Vertex> e : neighborEdges) {
+                    if (dequeuedList.contains(e.to())) { continue; }
 
-                Vertex p = e.from(), q = e.to();
-                double w = e.weight();
-                double distToP = distances.get(p);
-                double distToQ = distToP + w;
-                double qPlusH = distToQ + input.estimatedDistanceToGoal(q, end);
+                    Vertex p = e.from(), q = e.to();
+                    double w = e.weight();
+                    double distToP = distances.get(p);
+                    double distToQ = distToP + w;
+                    double qPlusH = distToQ + input.estimatedDistanceToGoal(q, end);
 
-                // If the goal vertex is reached, problem is solved.
-                if (q.equals(end)) {
-                    solution.add(q);
-                    solutionWeight += e.weight();
-                    outcome = SolverOutcome.SOLVED;
-                    timeSpent = sw.elapsedTime();
-                    finished = true;
-                }
+                    // If the goal vertex is reached, problem is solved.
+                    if (q.equals(end)) {
+                        solutionWeight += e.weight();
+                        outcome = SolverOutcome.SOLVED;
+                        timeSpent = sw.elapsedTime();
 
-                // If explored vertex is new, add to PQ.
-                if (!distances.containsKey(q)) {
-                    PQ.add(q, qPlusH);
-                    distances.put(q, distToQ);
-                }
+                        // Create the solutions list by iterating through the path
+                        // in reverse order using the origins HashMap.
+                        solution.add(q);
+                        solution.add(p);
+                        Vertex path = p;
+                        while (!solution.contains(start)) {
 
-                // Edge relax algorithm.
-                else if (distances.get(p) + w < distances.get(q)) {
-                    distances.put(q, distToQ);
-                    if (distances.containsKey(q)) {
-                        PQ.changePriority(q, qPlusH);
-                    } else {
+                            solution.add(origins.get(path));
+                            path = origins.get(path);
+                        }
+                        Collections.reverse(solution);
+                    }
+
+                    // If explored vertex is new, add to PQ.
+                    if (!distances.containsKey(q)) {
                         PQ.add(q, qPlusH);
+                        distances.put(q, distToQ);
+                        origins.put(q, p);
+                    }
+
+                    // Edge relax algorithm.
+                    else if (distances.get(p) + w < distances.get(q)) {
+                        distances.put(q, distToQ);
+                        origins.put(q, p);
+                        if (PQ.contains(q)) {
+                            PQ.changePriority(q, qPlusH);
+                        } else {
+                            PQ.add(q, qPlusH);
+                        }
                     }
                 }
-
             }
         }
     }
--- HW/HW4-AStarSolver/bearmaps/proj2ab/ArrayHeapMinPQ.java
+++ HW/HW4-AStarSolver/bearmaps/proj2ab/ArrayHeapMinPQ.java
@@ -12,7 +12,7 @@ import java.util.HashSet;
  * Date: 06/02/2019
  */
 
-public class ArrayHeapMinPQ<T extends Comparable<T>> implements ExtrinsicMinPQ<T> {
+public class ArrayHeapMinPQ<T> implements ExtrinsicMinPQ<T> {
 
     private int size;
     private ArrayList<Entry<T>> heap;
@@ -102,13 +102,13 @@ public class ArrayHeapMinPQ<T extends Comparable<T>> implements ExtrinsicMinPQ<T
     /* Helper method that swaps an Entry with its smaller child
      * until it reaches an appropriate spot. */
     private void sink(int k) {
-        if (k*2 > size() || (k*2+1 > size() && heap.get(k*2).getPriority() > heap.get(k).getPriority())) {
+        if (k*2 > size() || (k*2+1 > size() && heap.get(k*2).getPriority() >= heap.get(k).getPriority())) {
             return;
         } else if (k*2+1 > size() && heap.get(k*2).getPriority() < heap.get(k).getPriority()) {
             swap(k*2);
             return;
         }
-        if (heap.get(k*2).getPriority() < heap.get(k*2+1).getPriority() &&
+        if (heap.get(k*2).getPriority() <= heap.get(k*2+1).getPriority() &&
             heap.get(k*2).getPriority() < heap.get(k).getPriority()) {
             swap(k*2);
             sink(k*2);

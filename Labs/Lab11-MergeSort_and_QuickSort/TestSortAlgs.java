ee502dc56f195286d7f544365403c8d7237e893e
QuantuMope <43973701+QuantuMope@users.noreply.github.com>
2019-06-17 03:56:47 -0700

    Finalize tests for QuickSort and MergeSort.

--- Labs/Lab11-MergeSort_and_QuickSort/TestSortAlgs.java
+++ Labs/Lab11-MergeSort_and_QuickSort/TestSortAlgs.java
@@ -2,6 +2,7 @@ import edu.princeton.cs.algs4.Queue;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.util.Random;
 
 public class TestSortAlgs {
 
@@ -13,8 +14,19 @@ public class TestSortAlgs {
         quickSortTest.enqueue("Andrew");
         quickSortTest.enqueue("Carson");
         quickSortTest.enqueue("Zeke");
-        QuickSort.quickSort(quickSortTest);
+        Queue<String> sorted = QuickSort.quickSort(quickSortTest);
+        assertEquals(quickSortTest.size(), sorted.size());
         assertTrue(isSorted(quickSortTest));
+
+        Random rand = new Random();
+        Queue<Integer> secondQuickTest = new Queue<>();
+        for (int i = 0; i < 50; i++) {
+            int n = rand.nextInt(51);
+            secondQuickTest.enqueue(n);
+        }
+        Queue<Integer> sorted2 = QuickSort.quickSort(secondQuickTest);
+        assertEquals(secondQuickTest.size(), sorted2.size());
+        assertTrue(isSorted(sorted2));
     }
 
     @Test
@@ -28,6 +40,16 @@ public class TestSortAlgs {
         Queue<String> sorted = MergeSort.mergeSort(mergeSortTest);
         assertEquals(mergeSortTest.size(), sorted.size());
         assertTrue(isSorted(sorted));
+
+        Random rand = new Random();
+        Queue<Integer> secondMergeTest = new Queue<>();
+        for (int i = 0; i < 50; i++) {
+            int n = rand.nextInt(51);
+            secondMergeTest.enqueue(n);
+        }
+        Queue<Integer> sorted2 = MergeSort.mergeSort(secondMergeTest);
+        assertEquals(secondMergeTest.size(), sorted2.size());
+        assertTrue(isSorted(sorted2));
     }
 
     /**
